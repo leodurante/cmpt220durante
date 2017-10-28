4bc7bc65ece570fc16fa703d47a6bcda19590d5c
leodurante <31547967+leodurante@users.noreply.github.com>
2017-10-27 22:06:49 -0400

    lab 7

---
+++ labs/7/Lab10_3.java
@@ -0,0 +1,45 @@
+package lab7;
+
+public class Lab10_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+		class MyInteger{
+			//craete int named value
+			private int value;
+			
+			//create constructor
+			MyInteger (int value){
+				this.value = value;
+			}
+			
+			//return value
+			public int getValue() {
+				return value;
+			}
+			
+			//isEven
+			public int isEven() {
+				return isEven(value);
+			}
+			
+			//isOdd
+			public int isOdd() {
+				return isOdd(value);
+			}
+			
+			//isPrime
+			public int isPrime() {
+				return isPrime
+						
+			}
+			
+		}
+
+	}
+
+}
+
+
+	
+	
